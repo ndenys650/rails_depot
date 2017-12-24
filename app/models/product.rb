64de6cb297f524ad939fb2ef11892316f8385fb4
Nate Denys <nate_denys@yahoo.com>
2017-12-24 00:19:19 -0800

    added challenge validation
    test for minimum length of ten and added custom message utilized :too_short

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,18 @@
 class Product < ApplicationRecord
     validates :title, :description, :image_url, presence: true
+    
     validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    
     validates :title, uniqueness: true
-    validates :image_url, allow_blank: true, format: { 
+    validates_length_of :title, 
+    :length => {
+        :minimum => 10, 
+        :too_short => "is too short and must contain 10 characters"
+    }
+    
+    validates :image_url, 
+    allow_blank: true, 
+    format: { 
             with: %r{\.(gif|jpg|png)\Z}i,
             message: 'must be a URL for GIF, JPG or PNG image.'
     }
