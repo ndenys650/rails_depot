d1bb779043bedb6be12b038279abbd867c49a3d7
Nate Denys <nate_denys@yahoo.com>
2017-12-25 15:44:43 -0800

    create cart
    add on img click add to cart functionality, add "add to cart" button, create sessions and session counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+  
   def index
+    @count = increment_counter
     @products = Product.order(:title)
+    @time = Time.now
+    @shown_message = "You’ve been here #{@count} times" if session[:counter] >5
   end
 end
